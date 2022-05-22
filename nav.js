034d6b4fd97c89755819a78b5ecbd341e63f7260
FrothyPizza <frothy.pizza@gmail.com>
2022-05-22 14:34:06 -0500

    Place

--- nav.js
+++ nav.js
@@ -4,6 +4,13 @@ fetch('nav.html')
     let oldelem = document.querySelector("script#replace_with_navbar");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
+    oldelem.parentNode.replaceChild(newelem, oldelem);
+    // get tab title
+    let title = document.querySelector("title").innerHTML;
+    document.getElementById("nav-bar").childNodes.forEach(element => {
+        if(element.innerHTML == title) {
+            element.classList.add("active");
+        }
+    });
 });
 // <script id="replace_with_navbar" src="nav.js"></script>
